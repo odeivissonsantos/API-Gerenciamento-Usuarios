83591b83493b824323ca01b297487411dc9eed7a
odeivissonsantos <deivissonsantos@hotmail.com>
2021-09-01 21:53:59 -0300

    inicio do projeto

---
+++ src/main/java/com/odeivissonsantos/gerenciamentousuarios/FreeReunioesApp1Application.java
@@ -0,0 +1,13 @@
+package com.odeivissonsantos.gerenciamentousuarios;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FreeReunioesApp1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FreeReunioesApp1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/odeivissonsantos/gerenciamentousuarios/FreeReunioesApp1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.odeivissonsantos.gerenciamentousuarios;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FreeReunioesApp1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
